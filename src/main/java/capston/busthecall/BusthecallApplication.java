56779bdc971199e9bcda174d22a8a17142d92c7f
KimJinYeong <cheche903@naver.com>
2024-04-05 12:57:22 +0900

    Initial commit

---
+++ src/main/java/capston/busthecall/BusthecallApplication.java
@@ -0,0 +1,13 @@
+package capston.busthecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusthecallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusthecallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capston/busthecall/BusthecallApplicationTests.java
@@ -0,0 +1,13 @@
+package capston.busthecall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusthecallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
