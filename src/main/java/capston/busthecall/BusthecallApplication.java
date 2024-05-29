63dc34d86dd4248a4ba9b4497f77f017b5028a60
imjunghun <dlawndgns9@naver.com>
2024-05-29 14:53:54 +0900

    feat: firebase fcm을 통해 기사에게 푸시 알림 기능 구현
    공공 데이터 API에 10초 주기로 요청 보내기.
    해당 정거장 도착 전에 기사 앱으로 승차 인원, 하차 인원 보내기.

--- src/main/java/capston/busthecall/BusthecallApplication.java
+++ src/main/java/capston/busthecall/BusthecallApplication.java
@@ -2,8 +2,10 @@ package capston.busthecall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BusthecallApplication {
 
 	public static void main(String[] args) {
